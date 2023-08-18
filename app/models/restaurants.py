2a87bb45d2f41c579d6e3a8f9678dc10021db364
Tony Vongvone <tvongvone@gmail.com>
2023-08-17 23:09:38 -0700

    restaurants model

---
+++ app/models/restaurants.py
@@ -0,0 +1,24 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    price_range = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "city": self.city,
+            "state": self.state,
+            "price_range": self.price_range
+        }
