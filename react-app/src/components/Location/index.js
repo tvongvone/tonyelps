1cfb14fe2265d79eada36238500a05644749d3ae
Tony Vongvone <tvongvone@gmail.com>
2023-09-12 23:48:37 -0700

    location component

---
+++ react-app/src/components/Location/index.js
@@ -0,0 +1,11 @@
+import './location.css'
+import React from 'react'
+
+
+export default function Location() {
+    return (
+        <div>
+            <div>This is the location component</div>
+        </div>
+    )
+}
