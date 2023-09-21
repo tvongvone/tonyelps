8aa781bbf4fd47155249343c4ba6ee8f9a9d2047
Tony Vongvone <tvongvone@gmail.com>
2023-09-20 23:50:15 -0700

    homepage template

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -6,7 +6,8 @@ export default function HomePage() {
     return (
         <div className="homepage-container">
             <div className="homepage-content">
-                This is the Home Page
+                <>This will be the image</>
+                <div>This will be the content </div>
             </div>
         </div>
     )
