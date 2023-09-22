ab8461e9b5429d5cb59abcc9d496ecd83dae3513
Tony Vongvone <tvongvone@gmail.com>
2023-09-21 21:13:13 -0700

    forms

--- app/forms/business_form.py
+++ app/forms/business_form.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, IntegerField
 from wtforms.validators import DataRequired, Length
 
-class RestaurantForm(FlaskForm):
+class BusinessForm(FlaskForm):
     name = TextAreaField('name', validators=[DataRequired(), Length(max=255, min=1)])
     city = TextAreaField('city', validators=[DataRequired(), Length(max=255, min=1)])
     state = TextAreaField('state', validators=[DataRequired(), Length(max=255, min=1)])
